824316c6377724de77202c43d62409c57c20118b
danwakeem <danwakeem@gmail.com>
2021-02-03 07:30:06 -0600

    Added version query

--- context/app.context.js
+++ context/app.context.js
@@ -4,18 +4,29 @@ export const AppContext = React.createContext({});
 
 export const AppProvider = ({
   children,
+  query
 }) => {
   const [experimentId] = useState(process.env.NEXT_PUBLIC_EXPERIMENT_ID);
   const [versionAId] = useState(process.env.NEXT_PUBLIC_VARIATION_A);
   const [versionBId] = useState(process.env.NEXT_PUBLIC_VARIATION_B);
   const [currentVersion, setCurrentVersion] = useState();
+  let queryVersion;
+  if (query.version) {
+    if (query.version === 'a') {
+      queryVersion = versionAId;
+    } else if (query.version === 'b') {
+      queryVersion = versionBId;
+    }
+  }
 
   useEffect(() => {
-    if (process.browser) {
+    if (process.browser && !queryVersion) {
       const map = window.optimizely.get('state').getVariationMap();
       setCurrentVersion(map[experimentId]?.id);
+    } else if (queryVersion) {
+      setCurrentVersion(queryVersion);
     }
-  }, [process.browser]);
+  }, [process.browser, queryVersion]);
 
   return (
     <AppContext.Provider
--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,12 @@
 import 'antd/dist/antd.css';
+import { useRouter } from 'next/router';
 import { AppProvider } from './../context/app.context';
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter();
   return (
-    <AppProvider>
+    <AppProvider query={router.query}>
       <Component {...pageProps} />
     </AppProvider>
   )
